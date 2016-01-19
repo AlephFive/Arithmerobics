91580b50cfa5b6388ab9c59ff217400e122efc30
Brian Ma <spo2.spire.club@gmail.com>
2016-01-19 22:25:43 +0800

    edited homepage

--- www/js/index.js
+++ www/js/index.js
@@ -45,7 +45,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        window.location.href="about.html";
+        window.location.href="screens/main.html";
         
         
     }
---
+++ www/js/main.js
@@ -0,0 +1,68 @@
+function $(x) {return document.getElementById(x);}
+
+function firstRunInit(){
+    localStorage.setItem("lastMode", "timer")
+    initDatabase();
+}
+
+//MAINPAGE
+function chooseGameMode(obj, mode){
+    obj.style.backgroundColor="#1ec5e5";
+        localStorage.lastMode = mode;
+    refreshGameModeOptionColour();
+}
+
+function refreshGameModeOptionColour(){
+    if(localStorage.lastMode == "timer"){
+        //select
+        $(optionTimer).style.backgroundColor="DodgerBlue";
+        
+        //deselect
+        $(optionStrikes).style.backgroundColor="DarkGrey";
+    }
+    
+    if(localStorage.lastMode == "strikes"){
+        //select
+        $(optionStrikes).style.backgroundColor="DodgerBlue";
+        
+        //deselect
+        $(optionTimer).style.backgroundColor="DarkGrey";
+    }
+    
+}
+
+function initDatabase(){
+    
+}
+
+function prepGame(gamefile){
+    localStorage.setItem("gameDefinition", gamefile);
+    
+    if(localStorage.lastMode == "timer"){
+        window.location.href="gamePageTimer.html"
+    }
+    
+    if(localStorage.lastMode == "strikes"){
+        window.location.href="gamePageStrikes.html"
+    }
+}
+
+
+
+//INGAME
+function win(file) {
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {
+        console.log("read success");
+        console.log(evt.target.result);
+    };
+    reader.readAsText(file);
+};
+
+var fail = function(evt) {
+    console.log(error.code);
+};
+
+entry.file(win, fail);
+
+//CREATING CUSTOM GAME
